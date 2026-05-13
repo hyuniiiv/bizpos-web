92c466736a9ffce8fb39ae211d16d720185d93ea
hyuniiiv <hyuniiiv@gmail.com>
2026-05-13 17:24:25 +0900

    fix: align Authorization header format with Bizplay Java sample

--- lib/payment/bizplay.ts
+++ lib/payment/bizplay.ts
@@ -72,7 +72,7 @@ export class BizplayClient {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
-          'Authorization': `OnlineAK ${this.onlineAK}`,
+          'Authorization': `${this.onlineAK}`,
           'Connection': 'keep-alive',
         },
         body: JSON.stringify(requestBody),
